1a8d9c13c8162c8b45450de3bb487dd3d8a34f7b
VasjaVn <euro2012tovinnitsa@gmail.com>
2015-03-19 00:55:30 +0200

    add post method

--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
 module.exports = {
   "get": {
     "/hello": require("./controllers/helloController").getAction
-  }
+  },
+
+    "post": {
+        "/hello": require("./controllers/helloController").postAction
+    }
 };
