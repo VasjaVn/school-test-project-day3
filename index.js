cd895ec23c1497f8531654f899329552d677a207
VasjaVn <euro2012tovinnitsa@gmail.com>
2015-03-19 01:16:42 +0200

    change code of http of server

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
 "use strict";
 
-var http = require("http"),
-    url = require("url"),
-    routes = require("./routes");
 
+var http = require('http'),
+    url = require('url');
 
+var routes = require('./routes');
 
+function requestHandler( request, response ) {
 
-function requestHandler(request, response){
+    var requestUrl = url.parse( request.url, true );
+    var method = request.method;
 
-    var parsedUrl = url.parse(request.url, true),
-        method = request.method.toLowerCase();
+    if ( routes[method][requestUrl.pathname] ) {
+        routes[method][requestUrl.pathname]( request, response );
 
-
-
-
-    if(routes[method][parsedUrl.pathname]){
-        routes[method][parsedUrl.pathname](request, response, function(err){
-            if(err){
-                response.statusCode = 500;
-                response.write(JSON.stringify({
-                    "message": err.message,
-                    "status": "Internal Server Error"
-                }));
-                response.end();
-            }else{
-                response.end();
-            }
-        });
-    }else{
+    } else {
         response.statusCode = 404;
-        response.end("Not Found");
+        response.end("Page not found");
     }
 }
 
+var server = http.createServer( requestHandler );
 
-var server  = http.createServer(requestHandler);
-
-
-server.listen(3000);
+server.listen(8081);
